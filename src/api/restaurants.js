92886e6f273f5bde8dab66921d0e56e4591c7b3c
Jefferson Daniel <jeffersondanielss@gmail.com>
2019-05-13 07:13:03 -0300

    feat: ratings

--- src/api/restaurants.js
+++ src/api/restaurants.js
@@ -1,13 +1,19 @@
 const { mongoose, model } = require('node-restful')
 
 const RestaurantsSchema = mongoose.Schema({
-  fame: String,
   title: String,
   rating: Number,
   knowFor: String,
   ocasion: String,
   observations: String,
   date: { type: Date, default: Date.now },
+  ratings: {
+    service: Number,
+    environment: Number,
+    food: Number,
+    price: Number
+  }
 })
 
 module.exports = model('Restaurant', RestaurantsSchema)
+
