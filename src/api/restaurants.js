b806c35b8586dc94e2246469d48d935c772ba574
Jefferson Daniel <jeffersondanielss@gmail.com>
2019-05-13 14:04:27 -0300

    feat: add all fields

--- src/api/restaurants.js
+++ src/api/restaurants.js
@@ -6,12 +6,29 @@ const RestaurantsSchema = mongoose.Schema({
   knowFor: String,
   ocasion: String,
   observations: String,
+  waitTime: String,
   date: { type: Date, default: Date.now },
   ratings: {
     service: Number,
     environment: Number,
     food: Number,
     price: Number
+  },
+  address: {
+    street: String,
+    number: Number,
+    zipcode: String,
+    city: { type: String, default: 'São Paulo' },
+    state: { type: String, default: 'SP' },
+    country: { type: String, default: 'Brasil' }
+  },
+  recomendations: {
+    askNext: String,
+    neverAsk: String,
+    worth: Boolean,
+  },
+  convenience: {
+    wifi: Boolean
   }
 })
 
